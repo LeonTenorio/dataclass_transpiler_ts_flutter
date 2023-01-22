b2de6ed19500e3c6f35831deb89f91721ca9373c
Leon <leon_lts@hotmail.com>
2023-01-22 16:53:11 -0300

    feat: class reading comment yaml parameter

--- core/definition/class_definition.py
+++ core/definition/class_definition.py
@@ -7,35 +7,40 @@ sys.path.append(core_folder_path + '/utils/')
 
 from type_definition import TypeDefinition
 from yaml_utils import get_boolean_of_yaml
+from general_utils import nullable_text
 
 class ClassDefinition:
     def __init__(self, name, yaml_content):
         self.name = name
-        self.class_type, self.exportable, self.use_hive, self.ts_validation = self._get_class_definition(yaml_content)
+        self.class_type, self.comment, self.exportable, self.use_hive, self.ts_validation = self._get_class_definition(yaml_content)
     def _get_class_definition(self, yaml_content):
         class_type = None
         use_hive = False
         ts_validation = False
         exportable = False
+        comment = None
 
         effective_yaml_content = yaml_content
         if(type(effective_yaml_content)!=list):
             effective_yaml_content = [effective_yaml_content]
         for item in effective_yaml_content:
-            key = list(item.keys())[0]
-            content = item[key]
-            if(key == reserved_type):
-                class_type = TypeDefinition(content)
-            elif(key == reserved_exportable):
-                exportable = get_boolean_of_yaml(content)
-            elif(key == reserved_use_hive):
-                use_hive = get_boolean_of_yaml(content)
-            elif(key == reserved_ts_validation):
-                ts_validation = get_boolean_of_yaml(content)
+            for key in item.keys():
+                content = item[key]
+                if(key == reserved_type):
+                    class_type = TypeDefinition(content)
+                elif(key == reserved_exportable):
+                    exportable = get_boolean_of_yaml(content)
+                elif(key == reserved_use_hive):
+                    use_hive = get_boolean_of_yaml(content)
+                elif(key == reserved_ts_validation):
+                    ts_validation = get_boolean_of_yaml(content)
+                elif(key == reserved_comment):
+                    comment = content
         
-        return class_type, exportable, use_hive, ts_validation
+        return class_type, comment, exportable, use_hive, ts_validation
     def __str__(self):
         return 'ClassDefinition(name: ' + self.name + \
+               ', comment: ' + nullable_text(self.comment) + \
                ', type: ' + str(self.class_type) + \
                ', exportable: ' + str(self.exportable) + \
                ', useHive: ' + str(self.use_hive) + \
@@ -45,3 +50,4 @@ reserved_type = 'type'
 reserved_exportable = 'exportable'
 reserved_use_hive = 'useHive'
 reserved_ts_validation = 'tsValidation'
+reserved_comment = 'comment'
