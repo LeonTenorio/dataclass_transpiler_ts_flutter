be14a53ffa9fd8754803b7277a761f746db2df9a
Leon <leon_lts@hotmail.com>
2023-03-01 20:34:04 -0300

    feat: typescript synthesis using enums

--- core/synthesis/typescript_synthesis.py
+++ core/synthesis/typescript_synthesis.py
@@ -3,24 +3,62 @@ def ts_synthesis_of_elements(map_of_elements, element_key):
     
     element_name = element_definition.name
     class_type_definition = element_definition.class_type
+    enum_definition = element_definition.enum
     element_comment = element_definition.comment
     element_exportable = element_definition.exportable
 
     element_text = _create_comment(element_comment)
     if(element_exportable):
         element_text = element_text + 'export '
-    element_text = element_text + 'type ' + element_name + ' = ' + _ts_text_of_type_definition(class_type_definition) + '\n'
 
+    if(class_type_definition!=None):
+        element_text = element_text + _ts_text_of_class(element_name, class_type_definition)
+    else:
+        element_text = element_text + _ts_text_of_enum(enum_definition)
+    
     return element_text
+
+def _ts_text_of_class(
+    class_name,
+    class_type_definition
+):
+    return 'type ' + class_name + ' = ' + _ts_text_of_type_definition(class_type_definition)
+
+def _ts_text_of_enum(
+    enum_definition
+): 
+    enum_text = _create_comment(enum_definition.comment) + 'enum ' + enum_definition.name + ' {\n'
+    
+    for option in enum_definition.enum_options:
+        option_name = option.name
+        option_value = option.value
+        option_comment = option.comment
+
+        option_text = _create_comment(option_comment, '  ') + '  '
+
+        option_text = option_text + option_name
+        if(option_value!=None):
+            option_text = option_text + ' = ' + str(option_value)
+        option_text = option_text + ','
+
+        enum_text = enum_text + option_text + '\n'
+    
+    enum_text = enum_text + '}\n'
+
+    return enum_text
 
 def _ts_text_of_type_definition(type_definition, line_indentation = ''):
     simple_type = type_definition.simple_type
     if(simple_type!=None):
         return _get_ts_simple_type(simple_type)
     
+    additional_text = ''
     complex_type_text = '{\n'
     object_fields = type_definition.object_fields
     for object_field in object_fields:
+        object_field_enum = object_field.enum
+        if(object_field_enum!=None):
+            additional_text = additional_text + '\n' + _ts_text_of_enum(object_field_enum)
         object_field_name = object_field.name
         object_field_comment = object_field.comment
         if(object_field_comment != None):
@@ -29,7 +67,11 @@ def _ts_text_of_type_definition(type_definition, line_indentation = ''):
         object_field_optional = object_field.optional
         if(object_field_optional):
             complex_type_text = complex_type_text + '?'
-        object_field_ts_text_type = _ts_text_of_type_definition(object_field.type_definition, line_indentation = line_indentation + '  ')
+        object_field_ts_text_type = ''
+        if(object_field_enum!=None):
+            object_field_ts_text_type = object_field_enum.name
+        else:
+            object_field_ts_text_type = _ts_text_of_type_definition(object_field.type_definition, line_indentation = line_indentation + '  ')
         if(object_field_ts_text_type.endswith(';\n')):
             object_field_ts_text_type = object_field_ts_text_type[:-2]
         complex_type_text = complex_type_text + ': ' + object_field_ts_text_type
@@ -70,6 +112,8 @@ def _ts_text_of_type_definition(type_definition, line_indentation = ''):
             complex_type_text = complex_type_text + ')' 
     
     complex_type_text = complex_type_text + ';\n'
+
+    complex_type_text = complex_type_text + additional_text
     
     return complex_type_text
 
